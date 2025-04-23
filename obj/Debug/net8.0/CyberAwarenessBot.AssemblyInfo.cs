95b1fbfb23f24b7914d066d0b6ce4f25964263c0
ST10445541 <ST10445541@vcconnect.edu.za>
2025-04-23 19:07:56 +0200

    Updated assembly info and cache files

--- obj/Debug/net8.0/CyberAwarenessBot.AssemblyInfo.cs
+++ obj/Debug/net8.0/CyberAwarenessBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberAwarenessBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a75872f35bee3542d0bbaf862a22da1d4210f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberAwarenessBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberAwarenessBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
